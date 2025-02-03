bdea68a41003aa27650841d270de48a49d825da6
AshutoshRode <arode1999@gmail.com>
2025-02-03 07:48:21 +0530

    Project Uploaded

---
+++ src/Components/home/Social.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Social() {
+  return (
+    <div className="home__social">
+      <a href="https://www.instagram.com/rodeashutosh/" className="home__social-icon" target='_blank'>
+        <i className="uil uil-instagram"></i>
+      </a>
+      <a href="" className="home__social-icon" target='_blank'>
+        <i className="uil uil-dribbble"></i>
+      </a>
+      <a href="https://github.com/AshutoshRode" className="home__social-icon" target='_blank'>
+        <i className="uil uil-github-alt"></i>
+      </a>
+    </div>
+  )
+}
+
+export default Social
